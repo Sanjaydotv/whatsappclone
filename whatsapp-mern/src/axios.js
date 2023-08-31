08cffd1f010492285ec88b27176dbb7a9c9c5527
Sanjay V <sanjay.2021f@vitstudent.ac.in>
2023-08-31 22:26:32 +0530

    axios

--- whatsapp-mern/src/axios.js
+++ whatsapp-mern/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:"http://localhost:3001"
+    baseURL:"http://ec2-16-171-172-250.eu-north-1.compute.amazonaws.com:3001/"
 })
 
 export default instance;
